98f855545166789462a77567f2f0072b4ab0f88e
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-19 01:59:32 +0530

    refactor: extract post as a separate component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,6 @@
 import { GiSettingsKnobs } from "react-icons/gi";
-import { HiDotsHorizontal } from "react-icons/hi";
-import { GoComment } from "react-icons/go";
-import { BsSuitHeart, BsBookmark, BsShare } from "react-icons/bs";
-import { AsideLeft, AsideRight } from "../../component";
+
+import { AsideLeft, AsideRight, Post } from "../../component";
 
 export const Home = () => {
       return (
@@ -15,9 +13,7 @@ export const Home = () => {
                               <main className="w-full basis-2/3">
 
                                     <header className="flex p-4">
-                                          {/* TODO */}
                                           <h1 className="text-xl">Home</h1>
-            
                                     </header>
 
                                     <div className="border ml-3 flex px-5 py-3">
@@ -41,34 +37,8 @@ export const Home = () => {
                                           <GiSettingsKnobs className="fill-blue-600 stroke-0 hover:stroke-2 text-2xl" />
                                     </div>
 
-                                    {/* -- POSTS -- */}
+                                    <Post />
 
-                                    <div className="border ml-3 flex px-5 py-3 hover:bg-slate-100">
-
-                                          <div className="mt-3 w-12 h-12 text-lg flex-none">
-                                                <img src="https://i.pravatar.cc/300?img=12" className="flex-none w-12 h-12 rounded-full" alt="avatar" />
-                                          </div>
-
-                                          <div className="w-full px-4 py-3">
-
-                                                <div className="w-full flex justify-between">
-                                                      <h2 className="">Chris Levin @chrislevin22</h2>
-                                                      <HiDotsHorizontal className="cursor-pointer"/>     
-                                                </div>
-
-                                                <p className="py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis omnis laboriosam officia nemo praesentium natus ipsam! Nisi rerum, asperiores eius dignissimos nostrum, quos voluptatum voluptas impedit molestias suscipit omnis obcaecati!</p>
-
-                                                <p className="text-sm text-gray-600">6 hours ago</p>
-
-                                                <div className="flex justify-between pt-4">
-                                                      <BsSuitHeart className="text-xl cursor-pointer"/>
-                                                      <GoComment className="text-xl cursor-pointer"/>
-                                                      <BsBookmark className="text-xl cursor-pointer"/>
-                                                      <BsShare className="text-xl cursor-pointer"/>
-                                                </div>
-                                          </div>
-                                    </div>
-                                    
                               </main>
 
                               <AsideRight />
