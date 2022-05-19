153a9a45527d4259e67da6a006024c956cc8aa5f
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-19 22:20:38 +0530

    feat: Provide the Redux Store to React

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router} from "react-router-dom";
+import { Provider } from "react-redux";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-        <App />
-    </Router>
+      <Router>
+        <Provider store = { store }>
+          <App />
+        </Provider>
+      </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
