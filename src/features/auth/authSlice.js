9d21db55acc7f76650e7cca277a8a04ce7d78c9a
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-19 20:23:42 +0530

    feat: Add authSlice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "reduxjs/toolkit";
+
+const initialState = {
+      token: localStorage.getItem("Alcon_token")?.token || "",
+      userInfo: JSON.parse(localStorage.getItem("Alcon_user"))?.userInfo || {},
+      isLoading: false,
+}
+
+export const authSlice = createSlice({
+      name: "auth",
+      initialState,
+      reducers: {
+            signOutHandler: (state) => {
+                  localStorage.removeItem("Alcon_token");
+                  localStorage.removeItem("Alcon_user");
+                  state.token = "";
+                  state.userInfo = {};
+            },
+      },
+      extraReducers: {
+            [signInHandler.pending]: (state) => {
+                  state.isLoading = true;
+            },
+            [signInHandler.fulfilled]: (state, { payload }) => {
+                  state.isLoading = false;
+                  state.token = payload.encodedToken;
+                  state.userInfo = payload.foundUser
+            },
+            [signInHandler.rejected]: (state, { payload }) => {
+                  state.isLoading = false;
+            },
+
+            [signUpHandler.pending]: (state) => {
+                  state.isLoading = true;
+            },
+            [signUpHandler.fulfilled]: (state, { payload }) => {
+                  state.isLoading = false;
+                  state.token = payload.encodedToken;
+                  state.userInfo = payload.createdUser
+            },
+            [signUpHandler.rejected]: (state, { payload }) => {
+                  state.isLoading = false;
+            },
+      },
+});
+
+export const { signOut } = authSlice.actions;
+
+export default authSlice.reducer ;
