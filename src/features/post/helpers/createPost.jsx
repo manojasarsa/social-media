7730cb67244683538872306d006d21307dc3c816
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-23 16:19:44 +0530

    feat: add createPost() with API call

---
+++ src/features/post/helpers/createPost.jsx
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const createPost = createAsyncThunk(
+    "post/createPost",
+    async ({ postData, token }, { rejectWithValue }) => {
+        try {
+            const { status, data } = await axios.post(
+                "/api/posts",
+                { postData },
+                { headers: { authorization: token }}
+            )
+
+            if (status === 201) {
+                return data.posts;
+            }
+        } catch (err) {
+            return rejectWithValue(err.response.data.error[0]);
+        }
+    }
+);
