3845ec08aa6e4b45027c72e44c12432315c6403d
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-23 16:13:33 +0530

    feat: add getAllPosts() with API call

---
+++ src/features/post/helpers/getAllPosts.jsx
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAllPosts = createAsyncThunk(
+    "post/getPosts",
+    async(_, { rejectWithValue }) => {
+        try {
+            const { status, data } = await axios.get("/api/posts");
+            if(status === 200) {
+                return data.posts;
+            }
+        } catch (err) {
+            return rejectWithValue(err.response.data.error[0]);
+        }
+    }
+);
