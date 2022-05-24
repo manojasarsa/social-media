d7e2b4f4f360f0599d251ea98c41914812a543af
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-24 17:15:54 +0530

    feat: add bookmarkSlice

---
+++ src/features/bookmark/bookmarkSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getAllBookmarks, addToBookmark, removeFromBookmark } from "./helpers";
+
+const initialState = {
+    error: "",
+    isLoading: false,
+    bookmarks: [],
+}
+
+const bookmarkSlice = createSlice({
+    name: "post",
+    initialState,
+    reducers: {
+        allBookmarks: state => {
+            state.bookmarks = []];
+        }
+    },
+    extraReducers: {
+        [getAllBookmarks.pending]: (state) => {
+            state.isLoading = true;
+            state.error = "";
+        },
+        [getAllBookmarks.fulfilled]: (state, { payload }) => {
+            state.isLoading = false;
+            state.bookmarks = payload;
+        },
+        [getAllBookmarks.rejected]: (state, { payload }) => {
+            state.isLoading = false;
+            state.error = payload;
+        },
+
+        // Add to Bookmark
+
+        [addToBookmark.pending]: (state) => {
+            state.isLoading = true;
+            state.error = "";
+        },
+        [addToBookmark.fulfilled]: (state, { payload }) => {
+            state.isLoading = false;
+            state.bookmarks = payload;
+        },
+        [addToBookmark.rejected]: (state, { payload }) => {
+            state.isLoading = false;
+            state.error = payload;
+        },
+
+        // Remove from Bookmark
+
+        [removeFromBookmark.pending]: (state) => {
+            state.isLoading = true;
+            state.error = "";
+        },
+        [removeFromBookmark.fulfilled]: (state, { payload }) => {
+            state.isLoading = false;
+            state.bookmarks = payload;
+        },
+        [removeFromBookmark.rejected]: (state, { payload }) => {
+            state.isLoading = false;
+            state.error = payload;
+        },
+
+    }
+});
+
+export const { allBookmarks } = bookmarkSlice.actions;
+
+export default bookmarkSlice.reducer;
