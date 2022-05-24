f2cb20469690c23b32d680d5c3fc8ed611ce3392
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-24 17:17:17 +0530

    feat: update store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import userReducer from "../features/user/userSlice";
 import postReducer from "../features/post/postSlice";
+import bookmarkReducer from "../features/bookmark/bookmarkSlice";
 
 export default configureStore({
     reducer: {
         auth: authReducer,
         user: userReducer,
-        posts: postReducer
+        posts: postReducer,
+        bookmarks: bookmarkReducer
     }
 });
