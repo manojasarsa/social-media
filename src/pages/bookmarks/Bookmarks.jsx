5fa000826030890ac4c5084a9fb46a244e3a5ed6
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-26 04:08:46 +0530

    feat: make bookmark page responsive

--- src/pages/bookmarks/Bookmarks.jsx
+++ src/pages/bookmarks/Bookmarks.jsx
@@ -1,17 +1,22 @@
 import { AsideLeft, AsideRight, Post } from "../../component";
+import { Link } from "react-router-dom";
 
 export const Bookmarks = () => {
     return (
         <div>
-            <div className="flex mx-12 my-4">
-                <div className="mx-auto flex px-32 h-screen w-screen">
+            <div className="flex justify-center px-5 sm:px-32">
+                <div className="flex h-screen w-screen">
 
                     <AsideLeft />
 
-                    <main className="w-full basis-2/3">
+                    <main className="w-full sm:basis-2/3">
 
-                        <header className="flex p-4">
+                        <header className="p-4 hidden sm:flex">
                             <h1 className="text-xl">Bookmarks</h1>
+                        </header>
+
+                        <header className="text-xl font-bold flex py-4 text-blue-600 sm:hidden">
+                            <Link to="/home"> ALCON </Link>
                         </header>
 
                         <Post />
