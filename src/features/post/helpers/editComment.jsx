47e5df22bb4da9905473cdb6058e1700adede226
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-28 02:53:22 +0530

    feat: add editComment helper function

---
+++ src/features/post/helpers/editComment.jsx
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const editComment = createAsyncThunk(
+    "/post/editComment",
+    async ({ postId, token, commentData }, { rejectWithValue }) => {
+        try {
+            const { status, data } = await axios.post(
+                `/api/comments/edit/${postId}/${commentData._id}`,
+                { commentData },
+                { headers: { authorization: token }}
+            )
+
+            if (status === 201) {
+                return data.posts;
+            }
+        } catch (err) {
+            return rejectWithValue(err.response.data.errors[0]);
+        }
+    }
+);
